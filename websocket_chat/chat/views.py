0760af8846bf991837517f5061cd421bb62cdf78
Aleksandr <popkov.aleks.v@yandex.ru>
2022-04-24 21:32:45 +0500

    Fix template name

--- websocket_chat/chat/views.py
+++ websocket_chat/chat/views.py
@@ -6,4 +6,4 @@ def index(request):
     return render(request, 'chat/index.html', {})
 
 def room(request, room_name):
-    return render(request, 'chat/room.hetml', {'room_name': room_name})
+    return render(request, 'chat/room.html', {'room_name': room_name})
