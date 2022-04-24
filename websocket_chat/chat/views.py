11f377a846591c408aeab14a3d8b56a45f15c9a6
Aleksandr <popkov.aleks.v@yandex.ru>
2022-04-24 21:27:22 +0500

    Added view for room template

--- websocket_chat/chat/views.py
+++ websocket_chat/chat/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'chat/index.html')
+    return render(request, 'chat/index.html', {})
+
+def room(request, room_name):
+    return render(request, 'chat/room.hetml', {'room_name': room_name})
